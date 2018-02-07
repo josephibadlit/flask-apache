a9559ed3e1b0923107984815a1ecee68aac02fc0
josephibadlit <mrjosephibadlit@gmail.com>
2018-02-07 12:21:36 +0800

    Add endpoint to filter by state.

--- flaskapp.py
+++ flaskapp.py
@@ -49,6 +49,11 @@ def viewdb():
     rows = execute_query("""SELECT * FROM natlpark""")
     return '<br>'.join(str(row) for row in rows)
 
+@app.route("/state/<state>")
+def sortby(state):
+    rows = execute_query("""SELECT * FROM natlpark WHERE state = ?""",
+                         [state.title()])
+    return '<br>'.join(str(row) for row in rows)
 
 if __name__ == '__main__':
   app.run()
